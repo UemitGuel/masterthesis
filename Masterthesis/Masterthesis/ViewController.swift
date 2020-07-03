538f433135a891cd8d47714860c85ea7cb370e7d
quechupiOS <uemitgul@gmail.com>
2020-07-03 10:41:38 +0200

    add xcode project

---
+++ Masterthesis/Masterthesis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Masterthesis
+//
+//  Created by Ümit Gül on 03.07.20.
+//  Copyright © 2020 Ümit Gül. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
