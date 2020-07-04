9629244bfd510cc0e9526f1e63347433712a1656
quechupiOS <uemitgul@gmail.com>
2020-07-04 15:55:13 +0200

    TaskViewController UI

--- Masterthesis/TaskViewController.swift
+++ Masterthesis/TaskViewController.swift
@@ -10,21 +10,19 @@ import UIKit
 
 class TaskViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var startStudyButton: UIButton!
+    
+    @IBAction func startStudyButtonTapped(_ sender: UIButton) {
+        
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureButton()
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
 
+    func configureButton() {
+        startStudyButton.layer.cornerRadius = 10
+    }
 }
