0c47034cc27f9b9e32ad1392ffa9ae3d23c36b60
quechupiOS <uemitgul@gmail.com>
2020-07-04 15:46:23 +0200

    Email functionalty

--- Masterthesis/PersonalViewController.swift
+++ Masterthesis/PersonalViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 import ResearchKit
+import MessageUI
 
 class PersonalViewController: UIViewController {
-
+    
     @IBOutlet weak var showConsentDocument: UIButton!
     
     @IBAction func showConsentDocumentTapped(_ sender: UIButton) {
@@ -19,11 +20,39 @@ class PersonalViewController: UIViewController {
         present(taskViewController, animated: true, completion: nil)
     }
     
+    @IBOutlet weak var mailButton: UIButton!
+    
+    @IBAction func mailButtonTapped(_ sender: UIButton) {
+        sendEmail(subject: "Frage zur Umfrage um die psychische Gesundheit von Studenten zu evaluieren", body: "<p>Lieber Ümit,ich habe folgende Frage zu deiner Studie:</p>")
+    }
+    
+    @IBOutlet weak var eraseDataButton: UIButton!
+    
+    @IBAction func eraseDataButtonTapped(_ sender: UIButton) {
+        sendEmail(subject: "Studie aussteigen& Daten löschen", body: "<p>Lieber Ümit, bitte meine Daten umgehend löschen und mich aus der Studie entfernen</p>")
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureButton()
         // Do any additional setup after loading the view.
+    }
+    
+    func configureButton() {
+        showConsentDocument.layer.cornerRadius = 10
+        showConsentDocument.titleEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
+        showConsentDocument.contentEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
+        showConsentDocument.contentHorizontalAlignment = .left
+        mailButton.layer.cornerRadius = 10
+        mailButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
+        mailButton.contentEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
+        mailButton.contentHorizontalAlignment = .left
+        eraseDataButton.layer.cornerRadius = 10
+        eraseDataButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
+        eraseDataButton.contentEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
+        eraseDataButton.contentHorizontalAlignment = .left
     }
     
     func consentPDFViewerTask() -> ORKOrderedTask{
@@ -33,11 +62,31 @@ class PersonalViewController: UIViewController {
         PDFViewerStep.title = "Consent"
         return ORKOrderedTask(identifier: String("ConsentPDF"), steps: [PDFViewerStep])
     }
+    
+    func sendEmail(subject: String, body: String) {
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.setToRecipients(["uemit.guel@rwth-aachen.de"])
+            mail.setSubject(subject)
+            mail.setMessageBody(body, isHTML: true)
+            present(mail, animated: true)
+        } else {
+            print("fail")
+        }
+    }
 
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true)
+    }
 }
 
 extension PersonalViewController: ORKTaskViewControllerDelegate{
     func taskViewController(_ taskViewController: ORKTaskViewController, didFinishWith reason: ORKTaskViewControllerFinishReason, error: Error?) {
         taskViewController.dismiss(animated: true, completion: nil)
     }
+}
+
+extension PersonalViewController: MFMailComposeViewControllerDelegate {
+    
 }
