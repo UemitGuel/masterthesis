96f174cf353cf22e644453a69389a1c7c201a9ab
quechupiOS <uemitgul@gmail.com>
2020-07-11 15:21:23 +0200

    get daily steps

--- Masterthesis/TaskViewController.swift
+++ Masterthesis/TaskViewController.swift
@@ -29,11 +29,42 @@ class TaskViewController: UIViewController, HealthClientType {
         guard let healthStore = healthStore else { fatalError("healhStore not set") }
         let typesToRequest = Set<HKObjectType>(healthObjectTypes)
         healthStore.requestAuthorization(toShare: nil, read: typesToRequest) { (authorized, _) in
+            print(typesToRequest)
             guard authorized else { return }
+        }
+        updateSteps { double in
+            print(double)
         }
 
     }
     
+    func updateSteps(completion: @escaping (Double) -> Void) {
+        let stepsQuantityType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+
+        let now = Date()
+        let startOfDay = Calendar.current.startOfDay(for: now)
+        let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: now, options: .strictStartDate)
+
+        let query = HKStatisticsQuery(quantityType: stepsQuantityType, quantitySamplePredicate: predicate, options: .cumulativeSum) { (_, result, error) in
+            var resultCount = 0.0
+
+            guard let result = result else {
+                print("\(String(describing: error?.localizedDescription)) ")
+                completion(resultCount)
+                return
+            }
+
+            if let sum = result.sumQuantity() {
+                resultCount = sum.doubleValue(for: HKUnit.count())
+            }
+
+            DispatchQueue.main.async {
+                completion(resultCount)
+            }
+        }
+
+        healthStore?.execute(query)
+    }
     func configureButton() {
         startStudyButton.layer.cornerRadius = 10
     }
